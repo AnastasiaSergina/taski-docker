78d0ee78292cb396511fa61c494161210fa379dc
Анастасия Сергина <sergina.anastasia@gmail.com>
2024-09-30 20:23:02 +0300

    cj

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / '/frontend_static/static/'
+STATIC_ROOT = '/app/collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
